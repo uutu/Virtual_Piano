16008076acf224b7e046ce3f4f31503872da6bc3
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2020-06-24 21:54:57 +0200

    Add audio files, add creation of audio objects, finish requirements for Stage #4

--- Virtual Piano/task/src/script.js
+++ Virtual Piano/task/src/script.js
@@ -1,9 +1,33 @@
 document.addEventListener("keydown", function(event) {
-    if (event.code == "KeyA" || event.code == "KeyS" || event.code == "KeyD" || event.code == "KeyF"
-        || event.code == "KeyG" || event.code == "KeyH" || event.code == "KeyJ") {
-        console.log("The '" + event.code.charAt(3) +"' key is pressed");
+    if (event.code == "KeyA") {
+        let audio = new Audio("./audio/A.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyS") {
+        let audio = new Audio("./audio/S.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyD") {
+        let audio = new Audio("./audio/D.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyF") {
+        let audio = new Audio("./audio/F.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyH") {
+        let audio = new Audio("./audio/H.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyJ") {
+        let audio = new Audio("./audio/J.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyG") {
+        let audio = new Audio("./audio/G.mp3");
+        audio.play();
     }
     else {
-        console.log("Some other key was pressed");
+        console.log("The '" + event.code +"' key is pressed");
     }
 });
